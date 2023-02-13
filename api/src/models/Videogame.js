2c4d19e3358b0a41997cf7fdcea21d19d342e4cb
Evaristo Boquet <evaristoboquet@gmail.com>
2023-02-13 16:56:32 -0300

    creacion de modelos y db

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,38 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+module.exports = sequelize => {
+	// defino el modelo
+	sequelize.define('Videogame', {
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+			allowNull: false,
+			primaryKey: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		released: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		image: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		year: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		rating: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+	});
 };
